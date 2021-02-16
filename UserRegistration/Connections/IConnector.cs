6a6789fab9ddc9f7f7b99e5cedde1775c970b05c
Кирилл Димитриев <klokd2@gmail.com>
2021-02-16 17:10:23 +0300

    IConnector edded

---
+++ UserRegistration/Connections/IConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using UserRegistration.Models;
+
+namespace UserRegistration.Connections
+{
+    interface IConnector<T>
+    {
+        string login { get; set; }
+        public void Read(string login);
+        public void Save(UserDestinationModel userDestination);
+    }
+}
